9b05250968a38bc877e576484890ebbe96999ac2
PasanBhanu <pasanbguruge@hotmail.com>
2020-08-26 20:12:26 +0530

    Hospital List display

--- src/main/java/lk/spark/pasan/helpers/Http.java
+++ src/main/java/lk/spark/pasan/helpers/Http.java
@@ -5,9 +5,18 @@ import com.google.gson.JsonObject;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.io.Writer;
 
+/**
+ * Helper functions of API communication
+ */
 public class Http {
+    /**
+     * Set api response headers
+     *
+     * @param resp
+     * @param status
+     * @return
+     */
     public static HttpServletResponse setResponse(HttpServletResponse resp, int status) {
         resp.setStatus(status);
         resp.setContentType("application/json");
@@ -16,12 +25,27 @@ public class Http {
         return resp;
     }
 
-    public static PrintWriter getWriter(PrintWriter writer, String status, String message, JsonObject data, JsonArray errors) {
+    /**
+     * Add data to API response
+     *
+     * @param writer  Writer
+     * @param status  Status
+     * @param message Message
+     * @param data    Retrieved data
+     * @param errors  Errors
+     * @return
+     */
+    public static PrintWriter getWriter(PrintWriter writer, String status, String message, Object data, JsonArray errors) {
         JsonObject json = new JsonObject();
         json.addProperty("status", status);
         json.addProperty("message", message);
         if (data != null) {
-            json.add("data", data);
+            if (data instanceof JsonObject) {
+                json.add("data", (JsonObject) data);
+            } else {
+                json.add("data", (JsonArray) data);
+            }
+
         }
         if (errors != null) {
             json.add("errors", errors);
